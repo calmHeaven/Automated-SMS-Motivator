07a8f6c76bad65c3aa3b97c87858760a61b76a3b
ABENEZER TESSEMA <abenezer4989@gmail.com>
2024-02-26 09:55:07 -0600

    Phone Number Validator added via google libphonenumber 8.13.31 API

--- demo/src/main/java/com/example/demo/TwilioSmsSender.java
+++ demo/src/main/java/com/example/demo/TwilioSmsSender.java
@@ -3,10 +3,13 @@ package com.example.demo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
+import com.google.i18n.*;
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.twilio.type.PhoneNumber;
+import com.google.i18n.*;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.rest.api.v2010.account.MessageCreator;
-import com.twilio.type.PhoneNumber;
 
 @Service("twilio")
 public class TwilioSmsSender implements SmsSender {
@@ -21,21 +24,26 @@ public class TwilioSmsSender implements SmsSender {
 
     @Override
     public void sendSms(SmsRequest smsRequest) {
-        if(isPhoneNumberValid(smsRequest.getPhoneNumber())){
-        PhoneNumber to = new PhoneNumber(smsRequest.getPhoneNumber());
-        PhoneNumber from = new PhoneNumber(twilioConfiguration.getTrialNumber());
-        String message = smsRequest.getMessage();
-        MessageCreator creator = Message.creator(to, from, message);
-        creator.create();
-        LOGGER.info("Send sms" + smsRequest);
-    }
-    else{
-        throw new IllegalArgumentException("Phone number [" + smsRequest.getPhoneNumber() + "]is not vaid");
-    }
+        if (isPhoneNumberValid(smsRequest.getPhoneNumber())) {
+            PhoneNumber to = new PhoneNumber(smsRequest.getPhoneNumber());
+            PhoneNumber from = new PhoneNumber(twilioConfiguration.getTrialNumber());
+            String message = smsRequest.getMessage();
+            MessageCreator creator = Message.creator(to, from, message);
+            creator.create();
+            LOGGER.info("Send sms" + smsRequest);
+        } else {
+            throw new IllegalArgumentException("Phone number [" + smsRequest.getPhoneNumber() + "]is not valid");
+        }
     }
 
     private boolean isPhoneNumberValid(String phoneNumber) {
-        
-        return true;
+        PhoneNumberUtil phoneNumberUtil = PhoneNumberUtil.getInstance();
+        try {
+            com.google.i18n.phonenumbers.Phonenumber.PhoneNumber numberProto = phoneNumberUtil.parse(phoneNumber, "CA");
+            return phoneNumberUtil.isValidNumber(numberProto);
+        } catch (NumberParseException e) {
+            LOGGER.error("NumberParseException was thrown: " + e.toString());
+            return false;
+        }
     }
 }
